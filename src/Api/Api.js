08b714a7bfddb36b611571b17f54b8b2963eb26a
adryel martins <adryeldev@hotmail.com>
2024-09-30 16:34:33 -0300

    endereco api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-export const urlApi = 'http://localhost:8000/';
-//https://backendsistemasalao.onrender.com/
+export const urlApi = 'https://backendsistemasalao.onrender.com/';
 const Api = axios.create({
-    baseURL:'http://localhost:8000/',
-    //https://backendsistemasalao.onrender.com/
+    baseURL:'https://backendsistemasalao.onrender.com/',
 })
 
 
